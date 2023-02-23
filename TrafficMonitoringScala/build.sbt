96c94c99e0e1e97d387089262016f1c929086312
Bolot <bolot.89@gmail.com>
2023-02-23 21:15:00 +0100

    Java added to Traffic

--- TrafficMonitoringScala/build.sbt
+++ TrafficMonitoringScala/build.sbt
@@ -3,12 +3,16 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.13.10"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "3.2.2",
-  "org.apache.spark" %% "spark-sql" % "3.2.2",
-  "org.apache.spark" %% "spark-streaming" % "3.2.2",
+  "org.apache.spark" %% "spark-core" % "3.3.2",
+  "org.apache.spark" %% "spark-sql" % "3.3.2",
+  "org.apache.spark" %% "spark-streaming" % "3.3.2",
   "org.apache.logging.log4j" %% "log4j-api-scala" % "12.0",
-  "org.apache.logging.log4j" % "log4j-core" % "2.19.0"
+  "org.apache.logging.log4j" % "log4j-core" % "2.20.0"
+//  "org.opengis" %% "gt-xml" % "11.1"
 )
+
+//libraryDependencies  += "org.geotools" % "gt-shapefile" % "23.5"
+
 
 lazy val root = (project in file("."))
   .settings(
---
+++ TrafficMonitoringScala/src/main/scala/RoadModel/GPSRecord.java
@@ -0,0 +1,81 @@
+/**************************************************************************************
+ *  Copyright (c) 2019- Gabriele Mencagli and Alessandra Fais
+ *
+ *  This file is part of StreamBenchmarks.
+ *
+ *  StreamBenchmarks is free software dual licensed under the GNU LGPL or MIT License.
+ *  You can redistribute it and/or modify it under the terms of the
+ *    * GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version
+ *    OR
+ *    * MIT License: https://github.com/ParaGroup/StreamBenchmarks/blob/master/LICENSE.MIT
+ *
+ *  StreamBenchmarks is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  You should have received a copy of the GNU Lesser General Public License and
+ *  the MIT License along with WindFlow. If not, see <http://www.gnu.org/licenses/>
+ *  and <http://opensource.org/licenses/MIT/>.
+ **************************************************************************************
+ */
+
+package RoadModel;
+
+/**
+ *  @author Alessandra Fais
+ *  @version June 2019
+ *
+ *  The class defines a Point data structure containing X and Y coordinates.
+ */
+class Point {
+    double x;
+    double y;
+
+    Point() {
+    }
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+}
+
+/**
+ *  @author Alessandra Fais
+ *  @version June 2019
+ *
+ *  The class defines a GPSRecord data structure containing X and Y coordinates,
+ *  speed and direction.
+ */
+public class GPSRecord extends Point {
+    private double speed;
+    private int direction;
+
+    public GPSRecord() {
+    }
+
+    public GPSRecord(double x, double y, double speed, int direction) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+}
